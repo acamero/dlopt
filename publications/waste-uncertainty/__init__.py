9f29f0a913ff4be5ae5688469468557c7e68cab1
acamero <andrescamero@gmail.com>
2018-07-19 14:16:59 +0200

    Regularize master without waste-uncertainty

--- publications/waste-uncertainty/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file is part of DLOPT
-#
-# DLOPT is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of
-#    the License, or (at your option) any later version.
-__author__ = "Andrés Camero"
